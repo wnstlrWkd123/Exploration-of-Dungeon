f68c38def81f9f7b3c676012dff4cf1278f8f52f
wnstlrWkd123 <prkjnsk@naver.com>
2025-05-21 12:23:13 +0900

    feat: 점프대 추가
    점프대 기능 추가

---
+++ Assets/Scripts/Gimmick/JumpHelper.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JumpHelper : MonoBehaviour
+{
+    private float bounceForce = 25f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody _rigidbody = other.attachedRigidbody;
+        if (_rigidbody != null)
+        {
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z); // 기존 Y 속도 제거
+            _rigidbody.AddForce(Vector3.up * bounceForce, ForceMode.VelocityChange); // 즉시 튕겨올림
+        }
+    }
+}
